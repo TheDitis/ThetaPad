6f81c060b91cd54ff7ee3f49bdbbe7c1325ba6ad
Ryan McKay <ryanscottmckay@gmail.com>
2021-08-29 17:41:41 -0600

    Added many of the useful filters to the image. Have not yet applied them

--- src/Components/ThetaPad/Canvas/Layers/ImageLayer.tsx
+++ src/Components/ThetaPad/Canvas/Layers/ImageLayer.tsx
@@ -2,22 +2,61 @@
  * @file Konva Layer that holds the user's image
  * @author Ryan McKay <ryanscottmckay@gmail.com>
  */
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Image as KonvaImage, Layer as KonvaLayer} from "react-konva";
 import {imageLayerDimensionsSelector, imageSelector} from "../../../../redux/selectors";
 import useImage from "use-image";
 import {useAppSelector} from "../../../../redux/hooks";
-
+import Konva from "konva";
+import Image = Konva.Image;
 
 const ImageLayer: React.FC = () => {
     const imageData = useAppSelector(imageSelector);
     const dimensions = useAppSelector(imageLayerDimensionsSelector);
     const [image] = useImage(imageData.uri);
+    const imageRef = useRef<Image>(null);
 
+    useEffect(() => {
+        if (image && imageRef.current !== null) {
+            imageRef.current.cache();
+        }
+    }, [image])
 
     return (
         <KonvaLayer>
-            <KonvaImage image={image} width={dimensions.width} height={dimensions.height}/>
+            <KonvaImage
+                image={image}
+                ref={imageRef}
+                width={dimensions.width}
+                height={dimensions.height}
+                filters={[
+                    Konva.Filters.Contrast,
+                    Konva.Filters.Brighten,
+                    Konva.Filters.Enhance,
+                    Konva.Filters.HSV,
+                    Konva.Filters.HSL,
+                    Konva.Filters.Blur
+                ]} //, Konva.Filters.RGB]}
+                // blurRadius={50}
+                // brightness={-0.3}
+                // contrast={100}
+                // hue={100}
+                // saturation={-10}
+                // value={1}
+                // luminance={1}
+
+                // red, green, blue and alpha go together (RGBA filter)
+                // red={255}
+                // green={255}
+                // blue={255}
+                // alpha={0}
+
+
+
+                // enhance={10}
+                // kaleidoscopePower={4}
+                // kaleidoscopeAngle={45}
+            />
         </KonvaLayer>
     )
 }
