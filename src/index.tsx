4fb2926b3dd90c06e132d5eced27c501d90c37cc
Ryan McKay <ryanscottmckay@gmail.com>
2021-08-29 14:22:39 -0600

    setup firebase project

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,25 @@ import App from './Components//App/App';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/store";
 import {Provider} from "react-redux";
+import {FirebaseAppProvider} from "reactfire";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </FirebaseAppProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
