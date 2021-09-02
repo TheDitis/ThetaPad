ce5616b77c97afc8abf0c3508af11883b48ed6a9
Ryan McKay <ryanscottmckay@gmail.com>
2021-09-01 21:55:47 -0600

    updated todos

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -20,6 +20,7 @@ import {Provider} from "react-redux";
 // TODO: Add help button in navbar that shows info on the app
 // TODO: Cache store so users don't lose everything if they accidentally refresh
 // TODO: Add Layer to highlight 'selected' shapes
+// TODO: Style unit shape
 
 
 const theme = createTheme({
