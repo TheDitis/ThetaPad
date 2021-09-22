14d5505f4b8ad762d956741aabae6254f54e9149
Ryan <ryanscottmckay@gmail.com>
2021-09-22 09:37:47 -0600

    Updated Todos

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -18,10 +18,8 @@ import Overlays from "./Overlays/Overlays";
 // TODO: Resize shapes with image
 // TODO: Add help button in navbar that shows info on the app
 // TODO: Cache store so users don't lose everything if they accidentally refresh
-// TODO: Implement highlight for lengths and other shapes
-// TODO: Make poly-details dropdown appear only after drawing is done
+// TODO: Implement highlight for other shapes
 // TODO: Undo & redo
-// TODO: Style unit shape profile
 
 
 const theme = createTheme({
