2988266e68f710a07cfbba87d3645c689eb603d6
Ryan <ryanscottmckay@gmail.com>
2021-09-26 11:55:48 -0600

    Fixed key issue

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,8 @@ import App from './Components//App/App';
 import reportWebVitals from './reportWebVitals';
 import {FirebaseAppProvider} from "reactfire";
 
-console.log("KEY: ", "")
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCuLGF2vmTRjSxjljfKXRGiRg2uruH58Y0",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
@@ -17,8 +14,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
-
-// const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <React.StrictMode>
